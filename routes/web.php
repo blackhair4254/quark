779d1e4ef9bdf9c6f8cb91e69a12c8e2a647a279
blackhair4254 <220411100097@student.trunojoyo.ac.id>
2025-11-12 13:16:19 +0700

    route refresh token

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/wms/login', [WmsLoginController::class, 'show'])->name('wms.login')
 Route::post('/wms/login', [WmsLoginController::class, 'login'])->name('wms.login.post');
 Route::post('/wms/logout', [WmsLoginController::class, 'logout'])->name('wms.logout');
 Route::get('/shopee/get-access-token', [ShopeeAuthController::class, 'getAccessToken']);
+Route::post('/shopee/refresh-access-token', [\App\Http\Controllers\ShopeeAuthController::class, 'refreshAccessToken']);
+
 
 
 Route::middleware(['auth','role:wms'])->prefix('wms')->group(function () {
