f156ab54961682ffeb78a4d48d63bd687e471ec7
blackhair4254 <220411100097@student.trunojoyo.ac.id>
2025-12-04 11:34:07 +0700

    Detail Inbound

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,6 @@ Route::middleware(['auth','role:wms'])->prefix('wms')->group(function () {
         ->only(['index','create','store','show'])
         ->names('wms.inbound');
         // Aksi status
-    Route::get('/inbound/{inbound}', [OmsInboundController::class, 'show'])
-            ->name('inbound.show');
     Route::post('inbound/{inbound}/send',    [InboundController::class, 'send'])->name('wms.inbound.send');      
     Route::post('inbound/{inbound}/accept',  [InboundController::class, 'accept'])->name('wms.inbound.accept');  
     Route::post('inbound/{inbound}/confirm', [InboundController::class, 'confirm'])->name('wms.inbound.confirm');
@@ -124,6 +122,8 @@ Route::middleware(['auth','role:oms'])->prefix('oms')->group(function () {
     
     // START INBOUND
     Route::get('inbound', [OmsInboundController::class, 'index'])->name('oms.inbound.index');
+    Route::get('/inbound/{inbound}', [OmsInboundController::class, 'show'])
+        ->name('inbound.show');
     Route::post('inbound/{inbound}/accept',  [OmsInboundController::class, 'accept'])
         ->name('oms.inbound.accept');
     Route::post('inbound/{inbound}/confirm', [OmsInboundController::class, 'confirm'])
